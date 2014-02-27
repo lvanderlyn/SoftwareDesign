e26e7c22040af8a67060c7653602ad737b604a78
ddiggins <devynn.diggins@students.olin.edu>
2014-02-27 11:35:18 -0500

    Added Playfinder.py

---
+++ hw5/Playfinder.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 27 10:39:59 2014
+
+@author: ddiggins
+"""
+
+import re
+f = open('Shakespeare.txt')
+fullText = f.read()
+f.close
+
+def story_isolator(play):
+    end_of_boilerplate = play.index("all in black")
+    end_of_play = play.index("Enter PAGE")
+    playtext = play[end_of_boilerplate:end_of_play]
+    return playtext
+
+print story_isolator(fullText)
